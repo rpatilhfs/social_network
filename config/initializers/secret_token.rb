627081882018068823e679fcc015ead085560a45
Rahul Patil <rahulhfs@headfitted.com>
2012-08-02 14:28:10 +0530

    FirstCommit-AddBasicStuctureForRailsApplication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetwork::Application.config.secret_token = '892690857cea72883a832098252d75053b442e86b0bb70d7d846cde71aa778b43a367042626960d1c37dd3101c9162c993f62f6a79aa2d58dd0e38f2a02a2b6c'
